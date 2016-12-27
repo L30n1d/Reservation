109bbf2f7aa645bd173464d7409800f2a6892edc
L30n1d <leonid_a@yahoo.com>
2016-12-27 23:05:09 +0100

    Dodade listenerasdasdas

--- app/src/main/java/com/reservation/reservation/MainActivity.java
+++ app/src/main/java/com/reservation/reservation/MainActivity.java
@@ -1,12 +1,17 @@
 package com.reservation.reservation;
 
+import android.content.Intent;
+import android.graphics.Rect;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageButton imageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +19,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        imageButton = (ImageButton)findViewById(R.id.imageButton);
 
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              startActivity(new Intent(MainActivity.this, Main2Activity.class));
+            }
+        });
 
     }
 }
