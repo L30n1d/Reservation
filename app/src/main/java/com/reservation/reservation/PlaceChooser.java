47325f85c588e1308055a4bc66ca43b9ba3a209d
Nebojsha <nebojshaarsikj@gmail.com>
2016-12-28 19:44:49 +0100

    SplashActivity added test

---
+++ app/src/main/java/com/reservation/reservation/PlaceChooser.java
@@ -0,0 +1,17 @@
+package com.reservation.reservation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlaceChooser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place_chooser);
+    }
+}
