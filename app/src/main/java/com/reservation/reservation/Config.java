76cae0f541a5166d094e753161677eb910dd3cb5
L30n1d <leonid_a@yahoo.com>
2016-12-28 22:14:19 +0100

    Added Java classes.

---
+++ app/src/main/java/com/reservation/reservation/Config.java
@@ -0,0 +1,24 @@
+package com.reservation.reservation;
+
+/**
+ * Created by Leonid on 28.12.2016.
+ */
+
+public class Config {
+
+
+    public static final String ADD_CATEGORY="http://ucidoma.tk/Android2/addReservation.php";
+    public static final String GET_ALLCAFFES = "http://ucidoma.tk/Android2/getAllCaffes.php";
+    public static final String GET_ALLRESERVATION = "http://ucidoma.tk/Android2/getAllReservation.php";
+    public static final String GET_ALLRESERVATIONS ="http://ucidoma.tk/Android2/getAllReservations.php?id=";
+    public static final String GET_RESERVATION ="http://ucidoma.tk/Android2/getReservation.php?id=";
+    public static final String GET_SEATS = "http://ucidoma.tk/Android2/getSeats.php";
+
+
+    public static final String TAG_JSON_ARRAY="result";
+
+    public static final String TAG_ID="Id";
+    public static final String TAG_NAME="Name";
+
+
+}
--- app/src/main/java/com/reservation/reservation/MainActivity.java
+++ app/src/main/java/com/reservation/reservation/MainActivity.java
@@ -1,21 +1,100 @@
 package com.reservation.reservation;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Rect;
 import android.media.Image;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ListAdapter;
 import android.widget.RadioButton;
+import android.widget.SimpleAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageButton imageButton;
+    private String JSON_STRING;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        getJSON();
+
     }
+
+
+    private void showCaffe(){
+        JSONObject jsonObject = null;
+        ArrayList<HashMap<String,String>> list = new ArrayList<HashMap<String, String>>();
+        try {
+            jsonObject = new JSONObject(JSON_STRING);
+            JSONArray result = jsonObject.getJSONArray(Config.TAG_JSON_ARRAY);
+
+            for(int i = 0; i<result.length(); i++){
+                JSONObject jo = result.getJSONObject(i);
+                String id = jo.getString(Config.TAG_ID);
+                String name = jo.getString(Config.TAG_NAME);
+
+                HashMap<String,String> employees = new HashMap<>();
+                employees.put(Config.TAG_ID,id);
+                employees.put(Config.TAG_NAME,name);
+                list.add(employees);
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        ListAdapter adapter = new SimpleAdapter(
+                MainActivity.this, list, android.R.layout.simple_list_item_1,
+                new String[]{Config.TAG_NAME},
+                new int[]{android.R.id.text1});
+
+
+
+       // catList.setAdapter(adapter);
+    }
+
+    private void getJSON(){
+        class GetJSON extends AsyncTask<Void,Void,String> {
+
+            ProgressDialog loading;
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(MainActivity.this,"","Се вчитува...",false,false);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                JSON_STRING = s;
+                showCaffe();
+            }
+
+            @Override
+            protected String doInBackground(Void... params) {
+                RequestHandler rh = new RequestHandler();
+                String s = rh.sendGetRequest(Config.GET_ALLCAFFES);
+                return s;
+            }
+        }
+        GetJSON gj = new GetJSON();
+        gj.execute();
+    }
+
 }
