7bc0cd15153dce4bb89134001e462349be33a05e
Nebojsha <nebojshaarsikj@gmail.com>
2017-01-02 20:46:41 +0100

    Added seats to MAP2

---
+++ app/src/main/java/com/reservation/reservation/TestMap2.java
@@ -0,0 +1,13 @@
+package com.reservation.reservation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestMap2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_map2);
+    }
+}
